da857dbc90bfb94080c3ea667f51b2be01b00891
Teemu Rönkkö <31726090+teemuronkko@users.noreply.github.com>
2023-08-26 10:33:51 +0200

    New script to distribute jobs across multiple GPUs

---
+++ launch_af2_single.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+Help()
+{
+  echo ""
+  echo "Usage: $0 [input_fasta_path]"
+  echo ""
+  echo "  Submit an AlphaFold prediction job using a path to a fasta file."
+  echo ""
+  echo "  input_fasta_path : Path to a .fasta file containing multiple sequences."
+  echo ""
+}
+
+# Get the options
+while getopts ":h" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+   esac
+done
+
+# AlphaFold paths and conda environment
+AF_DIR="/workspace/alphafold-runpod"
+AFDB_DIR="/workspace/alphafold-genetic-databases"
+AF_ENV="/opt/conda/envs/af2_runpod"
+N_CPUS=$(grep -c ^processor /proc/cpuinfo)
+
+echo -e "Using ${N_CPUS} cpus"
+
+# Ouptut dir 
+AF_OUTPUT_DIR="/workspace/AF2_Models"
+mkdir -p $AF_OUTPUT_DIR
+
+# Go to AlphaFold 2 directory and activate required conda environment
+cd $AF_DIR
+source activate $AF_ENV
+
+# Get start time for logging
+start=`date +%s`
+
+# Read in fasta files from the list
+FASTAFILE=$1
+
+# Variable for tracking start time of the prediction
+pred_start=`date +%s`
+
+# Path to features.pkl file
+BASENAME=`basename $FASTAFILE .fasta`
+FEATURES_PKL="${AF_OUTPUT_DIR}/${BASENAME}/features.pkl"
+
+log_file="${AF_OUTPUT_DIR}/${BASENAME}/${BASENAME}_log.txt"
+    
+echo -e "[$(date)] >> Processing ${BASENAME}, ${i}/${N_LINES}"
+
+# Redirect standard output and standard error to the log file
+exec > "${log_file}" 2>&1
+
+echo -e "[$(date)] >> Job started at $(date) for ${BASENAME}"
+
+# Check if pickle file exists and start prediction if so
+if [ -e $FEATURES_PKL ]; then
+    echo -e "\n[$(date)] >> ***** Starting prediction for $BASENAME *****"
+
+    # Launch AF using the shell script
+    $AF_DIR/run_alphafold.sh -d $AFDB_DIR -o $AF_OUTPUT_DIR -f $FASTAFILE -t "2023-01-01" -g true -r true -e true -n ${N_CPUS} -m multimer -c full_dbs -p true -l 1 -b false
+else
+    echo -e "[$(date)] >> ***** Prediction is skipped for $FASTAFILE because $FEATURES_PKL does not exist. *****"
+fi
+
+if [ -f "$AF_OUTPUT_DIR/$BASENAME/ranked_0.pdb" ]; then
+    cp $AF_OUTPUT_DIR/$BASENAME/ranked_0.pdb $AF_OUTPUT_DIR/$BASENAME/${BASENAME}.pdb
+    pred_end=`date +%s`
+    pred_runtime=$((pred_end-pred_start))
+    echo -e "[$(date)] >> ***** Prediction for $BASENAME finished in $pred_runtime seconds. *****"
+elif [ -e $FEATURES_PKL ]; then 
+    echo -e "[$(date)] >> ***** Prediction failed, or $AF_OUTPUT_DIR/$BASENAME/ranked_0.pdb doesn't exist. *****"
+fi
+
+end=`date +%s`
+runtime=$((end-start))
+echo -e "[$(date)] >> AF2 finished at $(date) for $BASENAME"
+echo -e "[$(date)] >> Runtime: $runtime s"
+exec >&-
---
+++ parallelize_gpu_tasks.py
@@ -0,0 +1,50 @@
+from multiprocessing import Pool, current_process, Queue
+import os
+import subprocess
+
+# Get the current working directory
+cd = os.getcwd()
+
+# Set number of available GPUs and number of processes per GPU
+N_GPUS = 4
+PROCESS_PER_GPU = 1    
+
+# Empty queue to store GPU ids
+gpu_queue = Queue()
+
+# Helper function to launch AF2 on a GPU
+def launch_af2(filename):
+    gpu_id = gpu_queue.get()
+    try:
+        # Get the process id of current process
+        id = current_process().ident
+        print('Launching AF2 job with ID {} and input file {filename} starting on GPU {}'.format(id, filename, gpu_id))
+        
+        # Launch AF2 job on the GPU
+        AF2_command = f"bash {cd}/launch_af2_single.sh {filename}"
+        subprocess.run(AF2_command, shell=True)
+
+        # Print the process id of the finished job
+        print(f'Finished AF2 job with ID {id} for {filename} finished')
+
+    finally:
+        gpu_queue.put(gpu_id)
+
+# Fill the queue with GPU ids
+for gpu_ids in range(N_GPUS):
+    for _ in range(PROCESS_PER_GPU):
+        gpu_queue.put(gpu_ids)
+
+# Path to the text file containing the paths to the fasta files
+input_path = "path/to/your/file.txt"
+
+# Open the text file and read the paths to the fasta files
+with open(input_path, "r") as file:
+    fasta_paths = [ line.strip() for line in file ]
+
+# Create a pool of workers and launch the jobs on the pool
+pool = Pool(processes=PROCESS_PER_GPU * N_GPUS)
+for _ in pool.imap_unordered(launch_af2, fasta_paths):
+    pass
+pool.close()
+pool.join()
